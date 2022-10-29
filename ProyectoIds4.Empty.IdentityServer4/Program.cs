98c0554c11f02c97e9519b3226ec05f08254f3d6
DuvanSmith <dsmith.mr@gmail.com>
2022-10-29 17:33:26 -0500

    Mejora Ids

--- ProyectoIds4.Empty.IdentityServer4/Program.cs
+++ ProyectoIds4.Empty.IdentityServer4/Program.cs
@@ -1,14 +1,26 @@
 using ProyectoIds4.Empty.IdentityServer4AspNet.IdentityConfiguration;
+string MiCors = "MiCords";
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddIdentityServer()
-        .AddInMemoryClients(Clients.Get())
-        .AddInMemoryIdentityResources(Resources.GetIdentityResources())
-        .AddInMemoryApiResources(Resources.GetApiResources())
-        .AddInMemoryApiScopes(Scopes.GetApiScopes())
-        .AddTestUsers(Users.Get())
-        .AddDeveloperSigningCredential();
+builder.Services.AddCors(options => options.AddPolicy(name: MiCors, builder =>
+    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+
+builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+})
+    .AddInMemoryClients(Clients.Get())
+    .AddInMemoryIdentityResources(Resources.GetIdentityResources())
+    .AddInMemoryApiResources(Resources.GetApiResources())
+    .AddInMemoryApiScopes(Scopes.GetApiScopes())
+    .AddTestUsers(Users.Get())
+    .AddDeveloperSigningCredential();
+
+builder.Services.AddAuthentication();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -22,5 +34,6 @@ app.UseRouting();
 app.UseIdentityServer();
 app.UseAuthorization();
 app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
+app.UseCors(MiCors);
 
 app.Run();
