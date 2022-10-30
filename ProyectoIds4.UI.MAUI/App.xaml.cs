f1f408b2cfe0cfd839ce0b417117b52906484f99
Duvan Roque <dsmith.mr@gmail.com>
2022-10-30 13:19:09 -0500

    Added MAUI blazor

---
+++ ProyectoIds4.UI.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoIds4.UI.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
