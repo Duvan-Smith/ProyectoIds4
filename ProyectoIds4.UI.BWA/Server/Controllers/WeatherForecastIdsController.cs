861badd7ab1c793daa6030dd7766941a805cc465
Duvan Roque <dsmith.mr@gmail.com>
2022-11-01 19:30:31 -0500

    login error

--- ProyectoIds4.UI.BWA/Server/Controllers/WeatherForecastIdsController.cs
+++ ProyectoIds4.UI.BWA/Server/Controllers/WeatherForecastIdsController.cs
@@ -2,11 +2,13 @@
 
 using IdentityModel.Client;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ProyectoIds4.AppCore.IdentityServer4;
 using ProyectoIds4.Dto;
 using ProyectoIds4.UI.BWA.Server.HttpClienteWeatherForecast;
+using System.Security.Claims;
 
 namespace ProyectoIds4.UI.BWA.Server.Controllers;
 
@@ -26,6 +28,49 @@ public class WeatherForecastIdsController : ControllerBase
         _logger = logger;
         _identityServer4Service = identityServer4Service;
         _weatherForecastClientHttp = weatherForecastClientHttp;
+    }
+
+    [HttpGet(nameof(Login))]
+    public async Task Login()
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, "duvan"),
+            new Claim("UserName", "duvan"),
+            new Claim(ClaimTypes.Email, "dsmith.mr@gmail.com"),
+            new Claim(ClaimTypes.Role, "Administrator"),
+        };
+
+        var claimsIdentity = new ClaimsIdentity(
+            claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+        var authProperties = new AuthenticationProperties
+        {
+            AllowRefresh = false,
+            // Refreshing the authentication session should be allowed.
+
+            //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+            // The time at which the authentication ticket expires. A 
+            // value set here overrides the ExpireTimeSpan option of 
+            // CookieAuthenticationOptions set with AddCookie.
+
+            IsPersistent = false,
+            // Whether the authentication session is persisted across 
+            // multiple requests. When used with cookies, controls
+            // whether the cookie's lifetime is absolute (matching the
+            // lifetime of the authentication ticket) or session-based.
+
+            //IssuedUtc = <DateTimeOffset>,
+            // The time at which the authentication ticket was issued.
+
+            RedirectUri = "https://localhost:7167/counter"
+            // The full path or absolute URI to be used as an http 
+            // redirect response value.
+        };
+
+        await HttpContext.SignInAsync(CookieAuthenticationDefaults.LoginPath,
+            new ClaimsPrincipal(claimsIdentity),
+            authProperties).ConfigureAwait(false);
     }
 
     [HttpGet(nameof(GetToken))]
