e615ce2053d9767d4abd3ddd505a731e6e6f1b49
Duvan Roque <dsmith.mr@gmail.com>
2022-11-19 23:15:52 -0500

    Added ProyectoIds4.Cliente.BS

---
+++ ProyectoIds4.Cliente.BS/Program.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using ProyectoIds4.Cliente.BS.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignOutScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+})
+    .AddCookie()
+    .AddOpenIdConnect(options =>
+    {
+        options.Authority = "https://localhost:7132";
+        options.ClientId = "oidcMVCApp";
+        options.ClientSecret = "ProCodeGuide";
+        options.CallbackPath = "/";
+        options.ResponseType = "code";
+        options.SaveTokens = true;
+        options.Scope.Add("openid");
+        options.Scope.Add("weatherApi.read");
+        options.UsePkce = true;
+        options.ResponseMode = "query";
+        options.SignedOutRedirectUri = "https://localhost:7167/signin-oidc-callback";
+        options.TokenValidationParameters.ValidateIssuer = false;
+        options.TokenValidationParameters.NameClaimType = "name";
+    });
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
